481ef8035a95a6c7824549fa40b40b84ac981d1a
Oleg Kosenko <s6olkose@uni-bonn.de>
2020-02-14 18:00:04 +0100

    dealt with annoying errors

--- scripts/controller.py
+++ scripts/controller.py
@@ -14,10 +14,6 @@ class RobotControlClient:
 
 	def __init__(self):
 		rospy.init_node('listener', anonymous=True)
-		listener =  rospy.Subscriber('/tracker/pose', PoseStamped, self.robotCallback, queue_size=1)
-		listener2 = rospy.Subscriber('/cmd_vel', Params, self.commandCallback, queue_size=1)
-		listener3 = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.targetCallback, queue_size=1)
-		self.publisher = rospy.Publisher('/robomaster/info', PoseStamped, queue_size=1)
 
 		self.target = [-1., 0., 0.]
 		self.speed = [0.5, 0.1]
@@ -29,8 +25,16 @@ class RobotControlClient:
 		self.lastTime = 0
 
 		self.pitch = 0
-		self.globalYaw = 0
-		self.yaw = 0
+		self.baseYaw = 0
+		self.gimbalYaw = 0
+
+		self.p = 0
+		self.d = 0
+
+		listener =  rospy.Subscriber('/tracker/pose', PoseStamped, self.robotCallback, queue_size=1)
+		listener2 = rospy.Subscriber('/cmd_vel', Params, self.commandCallback, queue_size=1)
+		listener3 = rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.targetCallback, queue_size=1)
+		self.publisher = rospy.Publisher('/robomaster/info', PoseStamped, queue_size=1)
 
 		self.ws = websocket.WebSocketApp('ws://192.168.1.143:8181', on_message=self.on_message)
 		self.ws.run_forever()
@@ -42,7 +46,11 @@ class RobotControlClient:
 	def on_message(self, message):
 		msgList = message.split()
 		if len(msgList) == 3:
-			[self.pitch, self.globalYaw, self.yaw] = [float(x) for x in msgList]
+			self.pitch = np.radians(float(msgList[0]))
+			self.baseYaw = -np.radians(float(msgList[1]))
+			self.gimbalYaw = -np.radians(float(msgList[2]))
+		else:
+			rospy.loginfo(message)
 
 
 	def commandCallback(self, data):
@@ -63,7 +71,7 @@ class RobotControlClient:
 		theta = self.quat2Rotation(q)
 
 		self.target = [z, x, theta]
-		print(self.target)
+		rospy.loginfo('New target: ' + str(self.target))
 
 
 	def publish(self, movementVector, rotationCommand):
@@ -72,7 +80,7 @@ class RobotControlClient:
 		msg.pdcontrol.d = self.d
 		msg.pose.z = self.z
 		msg.pose.x = self.x
-		msg.pose.theta = self.robotAngle
+		msg.pose.theta = self.gimbalAngle
 		msg.sticks.lv = movementVector[0]
 		msg.sticks.lh = movementVector[1]
 		msg.sticks.rh = rotationCommand
@@ -80,8 +88,8 @@ class RobotControlClient:
 		msg.header.frame_id = "map"
 		msg.pose.position.x = self.z
 		msg.pose.position.y = self.x
-		msg.pose.orientation.z = np.sin(self.robotAngle/2.)
-		msg.pose.orientation.w = np.cos(self.robotAngle/2.)
+		msg.pose.orientation.z = np.sin(self.gimbalAngle/2.)
+		msg.pose.orientation.w = np.cos(self.gimbalAngle/2.)
 		self.publisher.publish(msg)
 
 
@@ -105,18 +113,18 @@ class RobotControlClient:
 
 
 	def quat2Rotation(self, q):
-		robotAngle = -np.arcsin(2*(q.x*q.z - q.w*q.y))
+		angle = -np.arcsin(2*(q.x*q.z - q.w*q.y))
 		robotFacingBackwards = 1-2*(q.y**2 + q.z**2) < 0
 		if robotFacingBackwards:
-			if robotAngle < 0:
-				robotAngle = -(np.pi + robotAngle)
+			if angle < 0:
+				angle = -(np.pi + angle)
 			else:
-				robotAngle = np.pi - robotAngle
-		return robotAngle
+				angle = np.pi - angle
+		return angle
 
 
 	def computeMovementVector(self):
-		robotForwardVector = np.array([np.cos(self.robotAngle), np.sin(self.robotAngle)])
+		robotForwardVector = np.array([np.cos(self.baseAngle), np.sin(self.baseAngle)])
 		robot2TargetVector = np.array([self.target[0] - self.z, self.target[1] - self.x])
 
 		robot2TargetAngle = np.arccos(np.dot(robotForwardVector, robot2TargetVector)/(np.linalg.norm(robotForwardVector)*np.linalg.norm(robot2TargetVector)))
@@ -133,18 +141,18 @@ class RobotControlClient:
 
 
 	def computeRotationCommand(self):
-		if np.abs(self.target[2] - self.robotAngle < np.pi):
-			rotationCommand = -(self.target[2] - self.robotAngle) * self.speed[1]
+		if np.abs(self.target[2] - self.gimbalAngle < np.pi):
+			rotationCommand = -(self.target[2] - self.gimbalAngle) * self.speed[1]
 		else:
-			rotationCommand = (self.target[2] - self.robotAngle) * self.speed[1]
+			rotationCommand = (self.target[2] - self.gimbalAngle) * self.speed[1]
 		return rotationCommand
 
 
 	def checkIfGoalReached(self):
 		positionGoalReached = np.abs(self.target[0] - self.z)<0.1 and np.abs(self.target[1] - self.x)<0.1
-		rotationGoalReached = np.abs(self.robotAngle - self.target[2])<0.035
+		rotationGoalReached = np.abs(self.gimbalAngle - self.target[2])<0.035
 		if positionGoalReached and rotationGoalReached:
-			rospy.loginfo('\nGoal Reached\n'+str(self.target))
+			rospy.loginfo_throttle(3, 'Goal Reached: '+str(self.target))
 		return positionGoalReached, rotationGoalReached
 
 
@@ -155,11 +163,16 @@ class RobotControlClient:
 		lostSignalCheck = np.abs(self.x) + np.abs(self.y) + np.abs(self.z) < 0.01
 		if lostSignalCheck:
 			msg = '0.0 0.0 0.0 0.0'
-			# rospy.loginfo('Lost Signal')
-			self.ws.send(msg)
+			rospy.loginfo_throttle(0.5, 'Lost Tracking Signal')
+			try:
+				self.ws.send(msg)
+			except:
+				rospy.loginfo_throttle(0.5, 'No WS Connection')
 			return
 			
-		self.robotAngle = self.quat2Rotation(data.pose.orientation)
+		self.gimbalAngle = self.quat2Rotation(data.pose.orientation)
+		self.baseAngle = self.gimbalAngle - self.gimbalYaw
+
 		movementVector = self.computeMovementVector()
 		rotationCommand = self.computeRotationCommand()
 
@@ -170,7 +183,11 @@ class RobotControlClient:
 			rotationCommand = 0.0
 
 		msg = str(movementVector[0]) + ' ' + str(movementVector[1]) + ' ' + str(0.0) + ' ' + str(rotationCommand)
-		self.ws.send(msg)
+		try:
+			self.ws.send(msg)
+		except:
+			rospy.loginfo_throttle(0.5, 'No WS Connection')
+		# rospy.loginfo(str(self.gimbalYaw))
 
 		self.publish(movementVector, rotationCommand)
 
